ee9301a3204b6a32c20bea639458c44e3f7c0af4
SineadManuel <c19336081@mytudublin.ie>
2021-04-21 12:35:15 +0100

    Resistor Constructor

--- src/ie/tudublin/Resistor.java
+++ src/ie/tudublin/Resistor.java
@@ -3,4 +3,12 @@ package ie.tudublin;
 public class Resistor {
     public int value, ones, tens, hundreds;
 
+    public Resistor(int value) {
+        this.value = value;
+
+        hundreds = (value / 100);
+        tens = (value - (hundreds * 100)) / 10;
+        ones = value - ((hundreds * 100)  + (tens * 10));
+    }
+
 }
